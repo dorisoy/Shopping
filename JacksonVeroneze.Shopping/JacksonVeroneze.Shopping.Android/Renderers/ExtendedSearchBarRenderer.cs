1fc4081e8f15958c06a35ffc7f4c08cda1f00f89
= <=>
2020-01-30 23:23:58 -0300

    Adicionado ícones do app e splash scream.

---
+++ JacksonVeroneze.Shopping/JacksonVeroneze.Shopping.Android/Renderers/ExtendedSearchBarRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(SearchBar), typeof(JacksonVeroneze.Shopping.Droid.Renderers.ExtendedSearchBarRenderer))]
+
+namespace JacksonVeroneze.Shopping.Droid.Renderers
+{
+    public class ExtendedSearchBarRenderer : SearchBarRenderer
+    {
+        public ExtendedSearchBarRenderer(Context context) : base(context: context) {  }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+        {
+            base.OnElementChanged(e);
+
+            if (this.Control == null) return;
+
+            var plateId = Resources.GetIdentifier("android:id/search_plate", null, null);
+
+            var plate = Control.FindViewById(plateId);
+
+            plate.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
---
+++ JacksonVeroneze.Shopping/JacksonVeroneze.Shopping/Converters/ValueToBrlConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace JacksonVeroneze.Shopping.Converters
+{
+    //
+    // Summary:
+    //     /// Class responsible for converting. ///
+    //
+    public class ValueToBrlConverter : IValueConverter
+    {
+        //
+        // Summary:
+        //     /// Method responsible to perform the conversion. ///
+        //
+        // Parameters:
+        //   value:
+        //     The value param.
+        //
+        //   targetType:
+        //     The targetType param.
+        //
+        //   parameter:
+        //     The parameter param.
+        //
+        //   culture:
+        //     The culture param.
+        //
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double total = (double)value;
+
+            return total.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"));
+        }
+
+        //
+        // Summary:
+        //     /// Method responsible to perform the conversion (inverse). ///
+        //
+        // Parameters:
+        //   value:
+        //     The value param.
+        //
+        //   targetType:
+        //     The targetType param.
+        //
+        //   parameter:
+        //     The parameter param.
+        //
+        //   culture:
+        //     The culture param.
+        //
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
