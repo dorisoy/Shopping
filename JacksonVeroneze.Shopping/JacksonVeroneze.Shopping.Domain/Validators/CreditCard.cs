edcafb1bc392435023b15e3ac718451de877d037
Jackson Veroneze <jackson@jacksonveroneze.com>
2020-02-10 00:03:02 -0300

    Correção de bugs na validação do cartão de crédito.

--- JacksonVeroneze.Shopping/JacksonVeroneze.Shopping.Domain/Validators/CreditCard.cs
+++ JacksonVeroneze.Shopping/JacksonVeroneze.Shopping.Domain/Validators/CreditCard.cs
@@ -7,10 +7,12 @@ namespace JacksonVeroneze.Shopping.Domain.Results
     {
         public static bool IsValid(string cardNo, string expiryDate, string cvv)
         {
-            Regex cardCheck = new Regex(@"^(1298|1267|4512|4567|8901|8933)([\-\s]?[0-9]{4}){3}$");
+            Regex cardCheck = new Regex(@"4[0-9]{6,}|5[1-5][0-9]{5,}|222[1-9][0-9]{3,}|22[3-9][0-9]{4,}|2[3-6][0-9]{5,}|27[01][0-9]{4,}|2720[0-9]{3,}|3(?:0[0-5]|[68][0-9])[0-9]{4,}|6(?:011|5[0-9]{2})[0-9]{3,}|(?:2131|1800|35[0-9]{3})[0-9]{3,}|3[47][0-9]{5,}");
             Regex monthCheck = new Regex(@"^(0[1-9]|1[0-2])$");
             Regex yearCheck = new Regex(@"^20[0-9]{2}$");
             Regex cvvCheck = new Regex(@"^\d{3}$");
+
+            cardNo = cardNo.Replace(" ", "");
 
             if (!cardCheck.IsMatch(cardNo))
                 return false;
